d61cea39683642b4a4c04535d2b5f9987db47ce6
Malo Chartier <malo@malo-ch.com>
2024-10-23 12:05:54 +0200

    push intermédiaire qui marche smr

--- apiWrapper.ts
+++ apiWrapper.ts
@@ -2,45 +2,53 @@
 let apiurl = "http://54.36.121.173:3012/v1/amazon"
 
 
-export type supportedCountry = "fr" | "de" | "it" | "es" | "co.uk"
+export type supportedCountry = "fr" | "de" | "it" | "es" | "co.uk" | "com.be"
 
-export type productResponse = {
+
+
+export type DefaultApiResponse<T extends string, D> = {
     status: "success" | "error",
     data: {
-        data: productData[],
-
+        [key in T]: D
+    } & {
+        clientName: string,
+        tokensLeft: number,
+        tokensUsed: number,
         responseTime: number,
-    }
-
+    },
     message: string,
+}
+
+
+
+
+
+export type searchData = {
+    query: string,
+    userCountry: supportedCountry,
+    productsData: productsData[],
+    totalSearchResults: number,
+    firstResult: number,
+    amount: number,
 
 }
 
-export type searchResponse = {
-    status: "success" | "error",
-    data: {
-        query : string,
-        userCountry: supportedCountry,
-        productsData : {
-            ASIN : string,
-            ResultsArray : productData[]
-        }[]
-
-        responseTime: number,
-    }
-
-    message: string,
-
+export type productsData = {
+    asin: string,
+    productData: productData[],
 }
+
 export type productData = {
     price: number,
     deliveryPrice: number,
     fullPrice: number,
+    priceNew: number,
+    priceUsed: number,
     availability: boolean,
     deliveryDate: number,
     country: supportedCountry,
     currencySymbol: string,
-    ASIN: string,
+    asin: string,
     techSpechs: object[],
     features: string[],
     productRating: number,
@@ -48,9 +56,23 @@ export type productData = {
     name: string,
     weight: number,
     brand: string,
+    reviews: number,
+    unitsSold: number,
+    variations: {
+        name: string,
+        selected: number,
+        options: {
+            asin: string,
+            value: string,
+            selected: boolean,
+        }[]
+    }[],
+    userComments: string[]
+    lastUpdated: Date
+
 }
 
-
+// verif si bien asin et en plus faut verif si pas plus de 100 asin dans bulk. Modifier prix asin bulk
 export class StellarScrape {
 
 
@@ -76,7 +98,7 @@ export class StellarScrape {
   * @param fromcountry The country of origin
   * @returns A promise resolving to the product data
   */
-    async getAmazonProduct(asin: string, countries: supportedCountry[], userCountry: supportedCountry): Promise<productResponse> {
+    async getAmazonProduct(asin: string, countries: supportedCountry[], userCountry: supportedCountry): Promise<DefaultApiResponse<'productData', productData[]>> {
 
         const url = `${apiurl}/product`;
         console.log(url);
@@ -98,15 +120,25 @@ export class StellarScrape {
         }
     }
 
-   /**
-  * Search on amazon for multiple products, in multiple countries
-  * @param query What to search for
-  * @param countries The list of countries to get items in
-  * @param fromcountry The country of origin
-  * @returns A promise resolving to the product data
-  */
-    async searchAmazonProducts(query: string, countries: supportedCountry[], userCountry: supportedCountry): Promise<searchResponse> {
+    /**
+   * Search on amazon for multiple products, in multiple countries
+   * @param query What to search for
+   * @param countries The list of countries to get items in
+   * @param fromcountry The country of origin
+   * @returns A promise resolving to the product data
+   */
+    async  searchAmazonProducts(
+        query: string, 
+        countries: supportedCountry[], 
+        userCountry: supportedCountry, 
+        amount?: number, 
+        startAt?: number
+      ): Promise<DefaultApiResponse<'searchData', searchData>> {
+        // Set default values inside the function if not provided
+        amount = amount ?? 10;
+        startAt = startAt ?? 0;
 
+        
         const url = `${apiurl}/search`;
         try {
             const response = await fetch(url, {
@@ -115,7 +147,46 @@ export class StellarScrape {
                     "Authorization": this.apikey,
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ query, countries, userCountry }),
+                body: JSON.stringify({ query, countries, userCountry , amount, startAt}),
+            });
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error fetching product data:', error);
+            throw new Error('Failed to fetch product data');
+        }
+    }
+
+
+
+
+
+    /**
+* Fetch multiple products details from the API
+* @param asin The ASIN of the products
+* @param countries The list of countries
+* @param fromcountry The country of origin
+* @returns A promise resolving to the product data
+*/
+    async getAmazonMultipleProducts(asinArray: string[], countries: supportedCountry[], userCountry: supportedCountry): Promise<DefaultApiResponse<'productsData', productsData[]>> {
+        if (asinArray.length > 100) {
+            throw Error("To much asin, the limit is 100 asin per requests")
+        }
+        if (!asinArray || !countries || !userCountry) {
+            throw Error("Missing one or more parameters")
+        }
+
+        const url = `${apiurl}/products`;
+        console.log(url);
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Authorization": this.apikey,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ asinArray, countries, userCountry }),
             });
             const data = await response.json();
             return data;
